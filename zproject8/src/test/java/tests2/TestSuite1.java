1c7a6f592ce404626023d6531c45a502c5cb7066
kishore <skill2automate@gmail.com>
2024-10-29 08:17:30 +0530

    nw cases added

---
+++ zproject8/src/test/java/tests2/TestSuite1.java
@@ -0,0 +1,33 @@
+package tests2;
+
+import org.junit.Test;
+
+import pages.HomePage;
+import pages.LoginPage;
+import utils.BrowserUtils;
+import utils.FrameWorkUtils;
+
+public class TestSuite1 {
+
+	@Test
+	public void testLogin() { 
+		
+		LoginPage loginPage= new LoginPage();
+		HomePage homePage = new HomePage();
+		FrameWorkUtils.setUpDriver();
+		BrowserUtils.launchBrowser();
+		BrowserUtils.invokeApp();
+		loginPage.verifyLoginPage();
+		loginPage.performlogin();
+		homePage.verifyHomepage();
+		homePage.logout();
+		homePage.logout();
+		homePage.verifyHomepage();
+		homePage.logout();
+		homePage.logout();
+		BrowserUtils.killBrowser();
+		
+		
+	}
+
+}
---
+++ zproject8/src/test/java/tests2/TestSuite3.java
@@ -0,0 +1,30 @@
+package tests2;
+
+import org.junit.Test;
+
+import pages.HomePage;
+import pages.LoginPage;
+import utils.BrowserUtils;
+import utils.FrameWorkUtils;
+
+public class TestSuite3 {
+
+	@Test
+	public void testLogin() { 
+		
+		LoginPage loginPage= new LoginPage();
+		HomePage homePage = new HomePage();
+		FrameWorkUtils.setUpDriver();
+		BrowserUtils.launchBrowser();
+		BrowserUtils.invokeApp();
+		loginPage.verifyLoginPage();
+		loginPage.performlogin();
+		homePage.verifyHomepage();
+		homePage.logout();
+		homePage.logout();
+		BrowserUtils.killBrowser();
+		
+		
+	}
+
+}
