7be4455621c3cc062c8d783e61a5587f4a6f2ae0
kishore <skill2automate@gmail.com>
2024-10-29 08:14:35 +0530

    added 3 filws

---
+++ zproject8/src/test/java/tests/TestSuite2.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.junit.Test;
+
+import pages.HomePage;
+import pages.LoginPage;
+import utils.BrowserUtils;
+import utils.FrameWorkUtils;
+
+public class TestSuite2 {
+
+	@Test
+	public void testLogin() { 
+		
+		LoginPage loginPage= new LoginPage();
+		HomePage homePage = new HomePage();
+		FrameWorkUtils.setUpDriver();
+		BrowserUtils.launchBrowser();
+		BrowserUtils.invokeApp();
+		loginPage.verifyLoginPage();
+		loginPage.performlogin();
+		homePage.verifyHomepage();
+		homePage.logout();
+		BrowserUtils.killBrowser();
+		
+		
+	}
+
+}
